52b92fd3b050cf06e32f5ee25c43f2016324f839
VuDN <vudn@rikai.technology>
2022-05-30 14:59:23 +0700

    Change Github Token

--- github_staff/Source/Enviroments/Enviroment.swift
+++ github_staff/Source/Enviroments/Enviroment.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 class Enviroment {
-    static let token = "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"
+    static let token = "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"
     static let host = "https://api.github.com"
 }
