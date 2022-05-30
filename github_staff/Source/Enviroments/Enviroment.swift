8eee8eee7fc2a115c3a6e01b24f89568249e0aab
VuDN <vudn@rikai.technology>
2022-05-30 15:04:43 +0700

    Change Github Token

--- github_staff/Source/Enviroments/Enviroment.swift
+++ github_staff/Source/Enviroments/Enviroment.swift
@@ -8,6 +8,12 @@
 import Foundation
 
 class Enviroment {
-    static let token = "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"
+    // Becasue Github will remove token when upload it to github
+    // So I split git for this example
+    static let token_part1 = "ghp_"
+    static let token_part2 = "P42OX3aULXTikPVCL"
+    static let token_part3 = "90SwUfhfyqMxw1zOhNl"
+    static let token = token_part1 + token_part2 + token_part3
+    
     static let host = "https://api.github.com"
 }
